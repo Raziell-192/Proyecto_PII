0bd2e9f3558c8d613c64bea1bb00c01ab96cdcaa
Jakim <joakinpinacho@gmail.com>
2025-11-17 19:54:32 -0600

    Agregué clase Main

--- src/Views/Login.java
+++ src/Views/Login.java
@@ -51,7 +51,7 @@ public class Login extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setTitle("UnsiSmile");
+        setTitle("Ingresar al sistema");
         setResizable(false);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import Views.Login;
+
+/**
+ *
+ * @author jakim
+ */
+public class Main {
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.setVisible(true);
+    }
+}
